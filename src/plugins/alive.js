72cdbfdf6149c8de2fa3a3c461eb2e4c5ccd4e56
𝗕𝗔𝗕𝗜𝗬𝗔 𝗜𝗗 <dheeshanadewmini12@gmail.com>
2026-06-05 12:46:44 +0530

    Update alive.js

--- src/plugins/alive.js
+++ src/plugins/alive.js
@@ -1,12 +1,11 @@
-module.exports = {
-    name: 'alive',
-    execute: async (sock, msg) => {
-        
-        const textMessage = msg.message?.conversation || msg.message?.extendedTextMessage?.text || '';
-        const jid = msg.key.remoteJid;
+sock.ev.on('messages.upsert', async (m) => {
+        const msg = m.messages[0];
+        if (!msg.message) return;
 
+        const jid = msg.key.remoteJid;
+        const textMessage = msg.message?.conversation || msg.message?.extendedTextMessage?.text || '';
+
+        
         if (textMessage.trim() === '.alive') {
             await sock.sendMessage(jid, { text: '*POWERED BY BABIYA MD* 🤖✨' }, { quoted: msg });
         }
-    }
-};
