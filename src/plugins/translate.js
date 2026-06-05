c713f4f3cf3e236ca00152674519589c24412ca3
𝗕𝗔𝗕𝗜𝗬𝗔 𝗜𝗗 <dheeshanadewmini12@gmail.com>
2026-06-05 12:44:04 +0530

    Update translate.js

--- src/plugins/translate.js
+++ src/plugins/translate.js
@@ -5,23 +5,22 @@ module.exports = {
         const jid = msg.key.remoteJid;
         if (!msg.key.fromMe) return;
 
-        if (textMessage.startsWith('.en ')) {
+    if (textMessage.startsWith('.en ')) {
             const textToTranslate = textMessage.replace('.en ', '').trim();
             if (textToTranslate) {
                 try {
+                    
                     const apiUrl = `https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=en&dt=t&q=${encodeURIComponent(textToTranslate)}`;
                     const response = await fetch(apiUrl);
                     const data = await response.json();
                     const translatedText = data[0][0][0];
 
                     if (translatedText) {
-                        // මැසේජ් එක Edit කරන කෑල්ල
+                        
                         await sock.sendMessage(jid, { text: translatedText, edit: msg.key });
                     }
                 } catch (err) {
                     console.log('Translate Error:', err);
                 }
             }
-        }
     }
-};
