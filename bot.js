1502c987397adb17fc6c06d1771a91b806011587
𝗕𝗔𝗕𝗜𝗬𝗔 𝗜𝗗 <dheeshanadewmini12@gmail.com>
2026-06-08 08:07:22 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ const killSwitches = new Map();
 const subscribedJids = new Set(); 
 
 async function startStatusBot() {
-    const { state, saveCreds } = await useMultiFileAuthState('status_auth_info');
+    const { state, saveCreds } = await useMultiFileAuthState('status_auth');
 
     const sock = makeWASocket({
         auth: state,
